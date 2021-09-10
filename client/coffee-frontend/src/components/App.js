1cc168d18d7c5daff362580b99319f8c54e5b4ca
Rakhim <abdurrohim9505@gmail.com>
2021-09-10 04:06:01 +0300

    Карточки добавил

--- client/coffee-frontend/src/components/App.js
+++ client/coffee-frontend/src/components/App.js
@@ -3,18 +3,20 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom'
 import { store } from '../redux/configureStore'
 import Footer from './footer/Footer';
 import Header from './headers/Header'
+import Cart from './Cart/Cart';
 
 
 function App() {
   return (
     <div className="App">
       <Provider store={store}>
-        <Header/>
+        <Cart />
+        {/*<Header/>*/}
         <BrowserRouter>
           <Switch>
             <Route path="/">
 
-              <Footer/>
+              {/*<Footer/>*/}
 
 
             </Route>
