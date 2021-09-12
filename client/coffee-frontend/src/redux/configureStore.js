41b82ebe7cfc1cd6a664184cf020302bc4f98bf7
Mamaev23 <ramzan95ramzan97@mail.ru>
2021-09-13 01:22:12 +0300

    реализовал регу чуток

--- client/coffee-frontend/src/redux/configureStore.js
+++ client/coffee-frontend/src/redux/configureStore.js
@@ -2,9 +2,11 @@ import { applyMiddleware, combineReducers, createStore } from 'redux'
 import thunk from "redux-thunk"
 import { createLogger } from 'redux-logger/src'
 import coffee from './features/Coffe'
+import authReducer from './features/Auth';
 
 const logger = createLogger()
 
 export const store = createStore(combineReducers({
-  coffee
+  coffee,
+  authReducer
 }), applyMiddleware(thunk, logger))
